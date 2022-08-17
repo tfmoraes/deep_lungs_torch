49bc96a963c9f415d1c8ffe4fc1e026fc822e915
Thiago Franco de Moraes <totonixsame@gmail.com>
2022-08-17 20:42:23 -0300

    Create volumes

--- create_volumes.py
+++ create_volumes.py
@@ -2,34 +2,12 @@ import argparse
 import pathlib
 import sys
 
+import imageio
 import nibabel as nib
 import numpy as np
 import pydicom
-import imageio
-
-parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument(
-    "-i",
-    "--input",
-    type=pathlib.Path,
-    metavar="folder",
-    help="Dicom input folder",
-    dest="input_folder",
-    required=True,
-)
-parser.add_argument(
-    "-o",
-    "--output",
-    default="output.vti",
-    type=pathlib.Path,
-    metavar="folder",
-    help="output folder",
-    dest="output_folder",
-    required=True,
-)
 
 ignore = ["ID00052637202186188008618"]
-
 
 def read_dicom_to_ndarray(folder: pathlib.Path) -> np.ndarray:
     dicom_arrs = []
@@ -38,7 +16,17 @@ def read_dicom_to_ndarray(folder: pathlib.Path) -> np.ndarray:
     return np.array(dicom_arrs)
 
 
-def save_to_nii(data: np.ndarray, filename: str):
+def read_images_to_ndarray(folder: pathlib.Path) -> np.ndarray:
+    image_files = list(folder.iterdir())
+    image_files.sort(key=lambda f: int(f.name.split('.')[0]))
+    images = []
+    for image_file in image_files:
+        image = imageio.imread(image_file)
+        images.append(image)
+    return np.array(images)
+
+
+def save_to_nii(data: np.ndarray, filename: pathlib.Path):
     img = nib.Nifti1Image(data, np.eye(4))
     nib.save(img, filename)
 
@@ -50,17 +38,36 @@ def create_screenshot(data: np.ndarray, filename: str):
 
 
 def main():
-    args, _ = parser.parse_known_args()
-    input_folder = args.input_folder.absolute()
-    output_folder = args.output_folder.absolute()
+    masks_folder = pathlib.Path('datasets/mask_clear/mask_clear/')
 
-    for dicom_folder in input_folder.iterdir():
-        if dicom_folder.name not in ignore:
-            dcm_array = read_dicom_to_ndarray(dicom_folder)
-            nii_filename = output_folder.joinpath(dicom_folder.name, "image.nii.gz")
-            nii_filename.parent.mkdir(parents=True, exist_ok=True)
-            save_to_nii(dcm_array, str(nii_filename))
-            #create_screenshot(dcm_array, str(nii_filename).replace("nii.gz", "png"))
+    images_train_folder = pathlib.Path('datasets/train')
+    images_test_folder = pathlib.Path('datasets/test')
+
+    images_output_folder = pathlib.Path('datasets/images')
+    masks_output_folder = pathlib.Path('datasets/masks')
+
+    images_output_folder.mkdir(parents=True, exist_ok=True)
+    masks_output_folder.mkdir(parents=True, exist_ok=True)
+
+    for mask_folder in masks_folder.iterdir():
+        if images_train_folder.joinpath(mask_folder.name).exists():
+            image_folder = images_train_folder.joinpath(mask_folder.name)
+        elif images_test_folder.joinpath(mask_folder.name).exists():
+            image_folder = images_test_folder.joinpath(mask_folder.name)
+        else:
+            print('err')
+            continue
+        print(image_folder)
+        dcm_array  = read_dicom_to_ndarray(image_folder)
+        images_array = read_images_to_ndarray(mask_folder)
+        save_to_nii(dcm_array, images_output_folder.joinpath(mask_folder.name).with_suffix('.nii.gz'))
+        save_to_nii(images_array, masks_output_folder.joinpath(mask_folder.name).with_suffix('.nii.gz'))
+        # if dicom_folder.name not in ignore:
+        #     dcm_array = read_dicom_to_ndarray(dicom_folder)
+        #     nii_filename = output_folder.joinpath(dicom_folder.name, "image.nii.gz")
+        #     nii_filename.parent.mkdir(parents=True, exist_ok=True)
+        #     save_to_nii(dcm_array, str(nii_filename))
+        #     #create_screenshot(dcm_array, str(nii_filename).replace("nii.gz", "png"))
 
 
 if __name__ == "__main__":
