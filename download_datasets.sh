c88d1a0f2192a9a85ed403138577e89791a85301
Thiago Franco de Moraes <totonixsame@gmail.com>
2022-08-17 20:44:35 -0300

    Added gitignore

--- download_datasets.sh
+++ download_datasets.sh
@@ -7,3 +7,6 @@ kaggle competitions download -c osic-pulmonary-fibrosis-progression -p datasets
 
 echo "Downloading masks"
 kaggle datasets download -d miklgr500/osic-pulmonary-fibrosis-progression-lungs-mask -p datasets/
+
+unzip datasets/osic-pulmonary-fibrosis-progression.zip -d datasets
+unzip datasets/osic-pulmonary-fibrosis-progression-lungs-mask.zip -d datasets
